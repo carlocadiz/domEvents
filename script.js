94729cfcff4877405edfde41dd3d8d373911a08e
Carlo Cadiz <carlocadiz71@gmail.com>
2019-07-24 14:25:53 -0400

    deleting list items, toggling line through class

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var ul = document.querySelector("ul");var li = document.querySelectorAll("li");
+//var list = document.querySelectorAll("li");
+
+
+function getCurrentList(){
+  const list = document.querySelectorAll("li");
+  return list;
+}
 
 function inputLength() {
   return input.value.length;
@@ -10,6 +17,7 @@ function createListElement(){
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+  createDeleteButton(getCurrentList().length - 1);
   input.value = "";
 }
 
@@ -25,7 +33,31 @@ function addListAfterKeypress(event){
   }
 }
 
+function createDeleteButton(index){
+  let list = getCurrentList();
+  let button=document.createElement("button");
+  button.appendChild(document.createTextNode("Delete!"));
+  list[index].appendChild(button);
+  button.onclick=removeParent;
+}
+
+function removeParent(event){
+  event.target.parentNode.remove();
+}
+
 button.addEventListener("click", addListAfterClick)
-
-
 input.addEventListener("keypress", addListAfterKeypress)
+
+ul.addEventListener("click", function(e){
+  var li = e.target.closest('li');
+  li.classList.toggle("done");
+})
+
+
+for( let i=0;i<getCurrentList().length;i++){
+  createDeleteButton(i);
+}
+
+
+
+
